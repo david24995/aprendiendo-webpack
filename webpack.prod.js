32da3e0a165dcbc81505167ffe96646cb58819fb
David Huamaccto <davidhuamaccto24995@gmail.com>
2020-02-24 17:51:00 -0500

    Finalizando la configuración de WebPack

---
+++ webpack.prod.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const minifyPlugin = require('babel-minify-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+    mode: 'production',
+    optimization: {
+        minimizer: [ new optimizeCssAssetsPlugin({}) ]
+    },
+
+    output: {
+        filename: 'main.[contentHash].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /style\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /style\.css$/,
+                use: [
+                    miniCssExtractPlugin.loader,
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_module/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+        new miniCssExtractPlugin({
+            filename: '[name].[contentHash].css',
+            ignoreOrder: false
+        }),
+        new minifyPlugin({}),
+        new CleanWebpackPlugin(),
+    ] 
+
+};
